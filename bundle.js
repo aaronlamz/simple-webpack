b61469cbec99eefbcb0985c30363fd61d3be0c46
linjiajun <linjiajun@youxin.com>
2020-04-06 17:10:22 +0800

    modify dir

--- bundle.js
+++ bundle.js
@@ -2,21 +2,35 @@ const fs = require("fs");
 const babylon = require("babylon");
 const traverse = require("babel-traverse").default;
 
+let ID = 0;
+
+// 解析一个文件及其依赖
 function createAsset(filename) {
   const content = fs.readFileSync(filename, "utf-8");
   const ast = babylon.parse(content, {
     sourceType: "module",
   });
-  // console.log("ast", ast);
 
-  const dependents = [];
+  const dependencies = [];
   traverse(ast, {
     ImportDeclaration: ({ node }) => {
-      // ??
-      // console.log("node", node);
-      dependents.push(node.source.value);
+      dependencies.push(node.source.value);
     },
   });
+
+  const id = ID++;
+
+  return {
+    id,
+    filename,
+    dependencies,
+  };
 }
 
-createAsset("./entry.js");
+// 构建一个依赖关系图
+function createGraph(entry) {
+  const mainAssets = createAsset(entry);
+}
+
+const graph = createAsset("./example/entry.js");
+console.log(graph);
